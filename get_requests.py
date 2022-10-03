3958945692b751d7fc0136470bc6641e74c840da
Lucas Jones <lucasejones+1@gmail.com>
2022-10-03 11:22:33 -0800

    Update get_requests.py
    Added the functionality to output the desired attributes

--- get_requests.py
+++ get_requests.py
@@ -2,73 +2,87 @@ import requests
 from bs4 import BeautifulSoup
 
 
-URL = "https://realpython.github.io/fake-jobs/"
-# URL = "https://info.nsf.org/Certified/Common/Company.asp?CompanyName=3m&Standard=061"
-page = requests.get(URL)
-soup = BeautifulSoup(page.content, "html.parser")
-
-# print(page.text)
-# print('\n' * 100)
-# print(soup.prettify())
-
-
+def create_soup(url):
+	page = requests.get(url)
+	soup = BeautifulSoup(page.content, "html.parser")
+	return soup
 
 # URL = "https://info.nsf.org/Certified/Common/Company.asp?CompanyName=3m&Standard=061"
-page = requests.get(URL)
-soup = BeautifulSoup(page.content, "html.parser")
-results = soup.find(id="ResultsContainer")
+stirred_soup = create_soup("https://realpython.github.io/fake-jobs/")
 
 
-# finding all the engineer jobs
-engineer_jobs = []
-for text in soup.find_all('a'):
-	job = text.get('href')
-	if 'engineer' in job:
-		engineer_jobs.append(job)
+def find_soup_results(soup):
+	results = soup.find(id="ResultsContainer")
+	return results
+
+soup_results = find_soup_results(stirred_soup)
 
 
+def get_job_elements(soupy_results: object) -> list:
+	"""
+	Takes a beautiful soup object.
+	Returns all jobs as a list of tuples, each containing the job title, the company name, and the location
+	"""
 
-# formatting the job postings that contain an engineering role
-import re 
+	job_elements = soupy_results.find_all('div', class_='card-content')
 
-stripped = []
-for job in engineer_jobs:
-	# print(job)
-	leading = "https://realpython.github.io/fake-jobs/jobs/"
-	useful = job[len(leading):]
-	useful = useful.replace('.html', '')
-	more_useful = useful.replace('-', ' ')
-	even_more_useful = re.sub(r'[0-9]', '', more_useful)
-	final = even_more_useful.strip()
-	stripped.append(final)
+	# for job_element in job_elements:
+	# 	print(job_element, end='\n' * 2) 
 
-# stripped = sorted(stripped)
-# print(stripped)
+	all_jobs = []
+	for job_element in job_elements:
+		title_element = job_element.find('h2', class_='title')
+		company_element = job_element.find('h3', class_='subtitle')
+		location_element = job_element.find('p', class_='location')
+		all_jobs.append((title_element.text.strip(), company_element.text.strip(), location_element.text.strip()))
 
-# fixing entries to not begin with engineer
-for job in stripped:
-	if job[:8] == 'engineer':
-		stripped.remove(job)
-		job = job.replace('engineer ', '')
-		job += ' engineer'
-		stripped.append(job)
+	return all_jobs
 
 
-stripped = sorted(stripped)
-# print(stripped)
-	
-for job in engineer_jobs:
-	print(job)
-
-for job in stripped:
-	print(job)
-
-print(len(stripped))
+all_processed_jobs = get_job_elements(soup_results)
+print(all_processed_jobs)
 
 
+def desired_jobs_only(scraped_jobs: list, *titles: str) -> list:
+	"""
+	Takes a list of jobs and an arbitrary number of job titles you'd like to identify from that list. 
+	Returns: A list of tuples containing only jobs relevant to the passedd titles
+	"""
 
-job_elements = results.find_all('div', class_='card-content')
-# print(job_elements.prettify())
-for job_element in job_elements:
-	print(job_element, end='\n' * 2) 
+	desired_jobs = []
+
+	for job in scraped_jobs:
+		for title in titles:
+			if title in job[0].lower():
+				desired_jobs.append(job)
+
+	return desired_jobs
+
+
+odd_jobs = desired_jobs_only(all_processed_jobs, 'broker', 'barrister', 'radiographer')
+dev_jobs = desired_jobs_only(all_processed_jobs, 'developer', 'programmer', 'software')
+
+
+## optional printed ouputs 
+# print('_' * 40)
+# print('All job titles: ', '\n')
+# print(all_processed_jobs)
+# print('_' * 40)
+# print('Desired jobs only: ', '\n')
+# print(desired_jobs_only(all_processed_jobs, 'broker', 'barrister', 'radiographer'))
+# print('_' * 40)
+
+
+def output_useful_job_details(desired_jobs: list) -> None:
+	"""
+	Takes the desired jobs as a list of tuples and prints it in a more readable format.
+	"""
+
+	print('_' * 40)
+	print('All matching open roles: ', '\n')
+	for role in desired_jobs:
+		print(role)
+
+
+print(output_useful_job_details(dev_jobs))
 
